8c2b44ccce65642bc3b07dcd55548dc16ad94a8f
Sam Scholefield <sam.scholefield@gmail.com>
2014-05-17 10:15:18 +0800

    3.x to 4.x express update
    edit for local dev
    ++require(‘connect’)
    ++switch express.method for connect.method

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 /**
  * Module dependencies.
  */
-require('newrelic');
+//require('newrelic');
 var express = require('express');
+var connect = require('connect');
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
@@ -13,11 +14,11 @@ var mongo = require('mongodb');
 var monk = require('monk');
 
 //uncomment for heroku
-var uri = process.env.MONGOLAB_URI;
-var db = monk(uri)
+//var uri = process.env.MONGOLAB_URI;
+//var db = monk(uri)
 
 //uncomment for local
-//var db = monk('localhost:27017/nodestrap');
+var db = monk('localhost:27017/nodestrap');
 
 var app = express();
 
@@ -25,17 +26,17 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(connect.favicon());
+app.use(connect.logger('dev'));
+app.use(connect.json());
+app.use(connect.urlencoded());
+app.use(connect.methodOverride());
+//app.use(app.router);
+app.use(connect.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(connect.errorHandler());
 }
 
 app.get('/', routes.index);
